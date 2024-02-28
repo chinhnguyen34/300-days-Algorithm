90e9886902d2fe4af154068f717b84b8766c73f6
Nguyễn Chính <115388632+chinhnguyen34@users.noreply.github.com>
2024-02-28 12:26:00 +0700

    Create day2-Valid Parentheses.cpp

---
+++ day2-Valid Parentheses.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for (char c : s) { 
+            if (c == '(' || c == '{' || c == '[') { 
+                st.push(c); 
+            } else {
+                if (st.empty() || 
+                    (c == ')' && st.top() != '(') || 
+                    (c == '}' && st.top() != '{') ||
+                    (c == ']' && st.top() != '[')) {
+                    return false; 
+                }
+                st.pop(); 
+            }
+        }
+        return st.empty(); 
+    }
+};
